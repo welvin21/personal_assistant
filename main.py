b22335fe2cc2e4c8ca80dc1873d2adf6cc5e4894
Welvin <welvin@Welvins-MacBook-Pro.local>
2019-08-10 23:32:44 +0800

    weather report optimization

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import webbrowser
 import random
 
 # try:
-#     userCommand = (r.recognize_google(audio))    
+#     userCommand = (r.recognize_google(audio))
 # except sr.UnknownValueError:
 #     speak("Sorry sir, I could not understand audio")
 # except sr.RequestError as e:
@@ -23,10 +23,10 @@ owm = OWM("328a3cfff5c381860add146bf8197513")
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-    
+
 def handleSpeech(userCommand):
     r = sr.Recognizer()
-    with sr.Microphone() as source : 
+    with sr.Microphone() as source :
         audio = r.listen(source)
         print("Listening....")
 
@@ -34,19 +34,19 @@ def findURL(userCommand):
     for i,word in enumerate(userCommand.split()):
         if(word=="open"):
             return("http://www."+userCommand.split()[i+1]+".com")
-    return "URL not found"
-    
+    return None
+
 
 def findLocation(userCommand):
     for country in pycountry.countries :
-        if country.name.lower() in userCommand : 
+        if country.name.lower() in userCommand :
             return country.name
     cities = geonamescache.GeonamesCache().get_cities()
     for key in cities :
         if cities[key]['name'].lower() in userCommand.split():
             return cities[key]['name'].capitalize()
     return None
-    
+
 def printAndSpeak(input):
     print(input)
     speak(input)
@@ -58,10 +58,9 @@ def displayWeather(weatherData):
     printAndSpeak("Status : "+weatherData.get_detailed_status())
 
 
-userCommand = ""  
+userCommand = ""
 while(True):
-    print("How may I help you, sir?")
-    speak("How may I help you sir?")
+    printAndSpeak("How may I help you, sir?")
     # userCommand = handleSpeech(userCommand)
     userCommand=input("Enter your command : ")
     if("bye" in userCommand.lower()):
@@ -70,12 +69,12 @@ while(True):
     elif(userCommand.lower()=="how are you doing"):
         speak(random.choice(["I am fine","Incredible, Sir","I am feeling great"]))
     elif("open" in userCommand.lower()):
-        speak("Ok sir, wait a second")
         URL = findURL(userCommand)
-        if(URL.lower()=="url not found"):
-            speak(URL)
+        if(URL==None):
+            speak("Sorry sir, couldn't find URL")
             continue
         else:
+            speak("Ok sir, opening " + URL +", wait a second")
             webbrowser.open(URL,new=2)
     elif("weather" in userCommand.lower()):
         location = findLocation(userCommand.lower())
@@ -85,7 +84,3 @@ while(True):
             printAndSpeak("Showing the weather report of "+location)
             weatherData = owm.weather_at_place(location).get_weather()
             displayWeather(weatherData)
-        
-        
-    
-
